27f6ab50fde730e8643805c66f3f1ea3473fe033
Brad M <bmcatee@gmail.com>
2024-01-09 18:52:34 -0600

    Minor cleanup

--- Assets/Scripts/Job.cs
+++ Assets/Scripts/Job.cs
@@ -24,11 +24,13 @@ public class Job : MonoBehaviour
     public int UpFrontPercentage { get; set; }
     // "Progress": A number that must be reduced to 0 via player rolls to complete the request; base value is defined by the job and then modified by Rank
     public int Progress { get; set; }
+    // "RollsTaken": This is how many rolls it's taken the Hire to complete the task; this is always a minimum of 1 and increments each time the Hire rolls without setting Progress to 0
+    public int RollsTaken { get; set; }
     // Stats: When the Hire attempts to complete a job request they roll their needed stat value against the Job's stat value as a pass/fail check
     // They then roll x-d(y) (x = number they beat their check roll by, y = their stat value) and remove that value from the Progress value
     // Example: Bob the Bowman has been hired to kill a bear. Bob uses Dexterity to attack, and the Bear uses Strength to defend.
     // Each rolls a D12 and adds the results to their relevant stat; Bob rolls a 9 and adds that to his 5 Dex, while the Bear rolls a 7 and adds that to its 3 strength. Bob has a total of 14, the Bear has a total of 10.
-    // Bob then rolls x-d(y), x = the number of points Bob beat the Bear's roll by, and y = Bob's Dexterity stat - or, 4d5. We then remove (4d5 + (Progress/5)) from the Progress value.
+    // Bob then rolls x-d(y), x = the number of points Bob beat the Bear's roll by, and y = Bob's Dexterity stat (or, 4d5). We then remove (4d5 + (Progress/5)) from the Progress value.
     // If Progress == 0, job done; if not, roll again
     // If Bob did NOT pass the initial check against the Bear, then the bear does damage to the Hire's HP for (1+Rank-d(Strength*Rank))
     public int Strength { get; set; }
--- Assets/Scripts/Keyword.cs
+++ Assets/Scripts/Keyword.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Keyword : MonoBehaviour
 {
-    public string Id { get; set; }
+    // public string Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
 }
